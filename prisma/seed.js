44691d2b17d6f4422a6337c6f04e3070cf8b87ca
al-ethea <alethea.azka3@gmail.com>
2025-08-08 23:28:13 +0700

    feat: seeding

---
+++ prisma/seed.js
@@ -0,0 +1,45 @@
+const bcrypt = require("bcrypt");
+const { PrismaClient, ShiftSession } = require("../src/generated/prisma");
+const prisma = new PrismaClient();
+
+const cashiers = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@example.com",
+    password: "password123", // will be hashed
+    shift: "SHIFT2",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Smith",
+    email: "jane@example.com",
+    password: "pass321",
+    shift: "SHIFT1",
+  },
+];
+
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+};
+
+async function main() {
+  for (const item of cashiers) {
+    const hashed = await hashPassword(item.password);
+    await prisma.cashier.create({
+      data: {
+        ...item,
+        password: hashed,
+      },
+    });
+  }
+}
+
+main()
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
