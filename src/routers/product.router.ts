4bdde47c0b2ba1751282445b19efc7be624c269c
al-ethea <alethea.azka3@gmail.com>
2025-08-23 21:23:38 +0700

    fix: edit cashier

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,13 +1,25 @@
 import { Router } from "express";
-import { getAllProducts, handleCreateProduct } from "../controllers/product.controller";
-import { createProductCategory, getAllProductCategories } from "../controllers/product.controller/product.category.controller";
+import {
+  deleteProductById,
+  getAllProducts,
+  getProductById,
+  handleCreateProduct,
+  updateProductById,
+} from "../controllers/product.controller";
+import {
+  createProductCategory,
+  getAllProductCategories,
+} from "../controllers/product.controller/product.category.controller";
 
 const productRouter = Router();
 
-productRouter.post('/create', handleCreateProduct)
-productRouter.get('/all', getAllProducts)
+productRouter.post("/create", handleCreateProduct);
+productRouter.get("/all", getAllProducts);
+productRouter.get("/id/:id", getProductById);
+productRouter.put("/update/:id", updateProductById);
+productRouter.delete("/delete/:id", deleteProductById);
 
-productRouter.post('/categories/create', createProductCategory)
-productRouter.get('/categories', getAllProductCategories)
+productRouter.post("/categories/create", createProductCategory);
+productRouter.get("/categories", getAllProductCategories);
 
 export default productRouter;
